cd5e4493362d559bea9581a1bc1d8e032899e237
Crowler 1337 <gamevax25@gmail.com>
2024-06-24 15:56:10 +0300

    Initial commit

---
+++ PractWork6/Games.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PractWork6
+{
+    public class Games
+    {
+        public int IdGame { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class SalaryConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"{(int)value:0,00} руб.";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
---
+++ PractWork6/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PractWork6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var games = new List<Games>()
+            {
+                new Games{IdGame = 1, Name = "Doom", Description = "Адское месилово с чертями", Category = "Shooter", Price= 1400},
+                new Games{IdGame = 2, Name = "Dota 2", Description = "Адское месилово с бомжами", Category = "MOBA",Price= 0},
+                new Games{IdGame = 3, Name = "HellDivers 2", Description = "КУПИТЕ ЭТО Эдику", Category = "Shooter", Price= 790},
+                new Games{IdGame = 4, Name = "The Elder Scrolls V: Skyrim", Description = "Порадуйте беседку", Category = "RPG", Price= 2200},
+            };
+            gamesDataGrid.ItemsSource = games;
+
+            var categories = new List<string>()
+            {
+                "RPG",
+                "MOBA",
+                "Shooter"
+            };
+            categoryComboBox.ItemsSource = categories;
+        }
+
+
+    }
+}
+
